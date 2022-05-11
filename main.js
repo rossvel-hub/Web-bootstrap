a59ebe3e3f04268b91a502e559f0b9288db52251
rossvel-hub <ommigos@gmail.com>
2022-05-11 10:48:02 -0500

    pagina web con bootstrap

---
+++ main.js
@@ -0,0 +1,11 @@
+// const app = document.getElementById('typewriter');
+ 
+// const typewriter = new Typewriter(app, {
+//   loop: false,
+//   delay: 75,
+// });
+ 
+// typewriter
+//   .typeString('Hola')
+//   .pauseFor(200)
+//   .start();
